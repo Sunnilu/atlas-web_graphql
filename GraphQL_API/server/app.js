c700c3bcd4a1b0216b85256a625da2c247b3a22a
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-12 12:51:20 -0500

    require the schema.js and add schema in an object we pass to the graphqlHTTP

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,39 +1,15 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
-const TaskType = require('./schema'); // Import the TaskType from schema.js
 
 const app = express();
 
-// Define the schema, now using TaskType
-const schema = buildSchema(`
-  type Query {
-    task: Task
-  }
-
-  type Task {
-    id: String
-    title: String
-    weight: Int
-    description: String
-  }
-`);
-
-// Root resolver
-const root = {
-  task: () => ({
-    id: '1',
-    title: 'Sample Task',
-    weight: 10,
-    description: 'This is a description for the task.'
-  })
-};
+// Import the schema from schema.js
+const schema = require('./schema');
 
 // Setup the GraphQL endpoint
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
-  graphiql: true, // Enable GraphiQL UI in the browser
+  graphiql: true
 }));
 
 // Start the server
