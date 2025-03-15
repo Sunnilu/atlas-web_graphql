7db97451216548efc70cb100b6715438fd5cbf45
Suntha <Suntha.Lucas@atlasschool.com>
2025-03-14 19:46:02 -0500

    to work with the apollo client

---
+++ GraphQL_API/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Apollo React App</h1>
+      {/* You can add more functionality and GraphQL queries here */}
+    </div>
+  );
+};
+
+export default App;
